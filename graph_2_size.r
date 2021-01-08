01356710e19acb156a45cd68645cf3ca61021c57
usernamedjpris <jeremie.gantet@gmail.com>
2021-01-08 07:11:45 +0100

    comments and legend

--- graph_2_size.r
+++ graph_2_size.r
@@ -1,32 +1,35 @@
+# importation des librairies
 library(readr)
 library(plyr)
 library(tidyverse)
 library(ggplot2)
-
-PATH = paste(dirname(rstudioapi::getSourceEditorContext()$path), 
-             "/dataset/smartphones.csv", sep = "")
+# chargement du dataset
+PATH = paste(dirname(rstudioapi::getSourceEditorContext()$path),"/dataset/smartphones.csv", sep = "")
 dfa<- read_csv2(PATH)
 
 dfa$Année <- format(as.POSIXlt(dfa$`date début commercialisation`,tz = "", "%d/%m/%Y"), format="%Y")
 
 
-
+# aggrégation par année selon les moyennes
 df<-ddply(dfa, "Année", summarize, som=sum(`masse (g)`>=0),masse=mean(`masse (g)`), largeur_carree = sqrt(mean(`largeur (mm)`)*mean(`hauteur (mm)`)), largeur=mean(`largeur (mm)`), hauteur=mean(`hauteur (mm)`), epaisseur=mean(`epaisseur (mm)`), surface=mean(`surface utile (%)`),ratio=mean(`largeur (px)`)/mean(`hauteur (px)`)) 
+
+# constatation du manque de donnée en 2010 et 2011
 ggplot(df,aes(x=Année, y=som))+geom_col()+geom_text(aes(label=som), vjust=1.6, color="black", size=3.5)+theme_minimal()
-ggplot(df,aes(x=Année, y=hauteur, fill=-epaisseur))+geom_col()
-ggplot(df,aes(x=Année, y=surface))+geom_col()
-ggplot(df,aes(x=Année, y=ratio))+geom_col()
+
+#tests
+#ggplot(df,aes(x=Année, y=hauteur, fill=-epaisseur))+geom_col()
 
 
-y_lim <- c(0,210)
-x_lim <- c(2010,2020.2)
+
+y_lim <- c(0,210) #intervalle (masse en g)
+x_lim <- c(2012,2020) #intervalle (années)   
 plot.new() 
-plot(df$Année, df$masse,col="#22222a",xlab="Années",ylab="← Poids (mg)",pch=15,cex=df$largeur_carree^2/1000,xlim=x_lim,ylim=y_lim)
-grid(nx = 0, ny = 100, col = "#b0d6ee", lty = "solid",lwd = par("lwd"), equilogs = TRUE)
+#affichage des contours de telephones
 par(new = T)
-plot(df$Année, df$masse,col="#22222a",xlab="Années",ylab="masse (mg) →",pch=15,cex=df$largeur_carree^2/1000,xlim=x_lim,ylim=y_lim)
+plot(df$Année, df$masse,col="#22222a",xlab="Années",ylab="masse (g) →",pch=15,cex=df$largeur_carree^2/1000,xlim=x_lim,ylim=y_lim) #largeur_carree mis au carre 
+#affichage par dessus des écrans 
 par(new = T)
-plot(df$Année,df$masse,col="#b0d6ee",xlab="Années",ylab="masse (mg) →",pch=15,cex=df$largeur_carree^2*df$surface/100/1000,xlim=x_lim,ylim=y_lim) #/20=*5/100
-
+plot(df$Année,df$masse,col="#b0d6ee",xlab="Années",ylab="masse (g) →",pch=15,cex=df$largeur_carree^2*df$surface/100/1000,xlim=x_lim,ylim=y_lim) #/100 pour pourcentage 
+#NB: la légende a été rajoutée à posteriori
 
 
--- graph_3_sensors.r
+++ graph_3_sensors.r
@@ -1,3 +1,4 @@
+# importation des librairies
 library(readr)
 library(plyr)
 library(tidyverse)
@@ -5,13 +6,13 @@ library(ggplot2)
 library(viridis) 
 library(fmsb)
 library(colormap)
+# chargement du dataset
 PATH = paste(dirname(rstudioapi::getSourceEditorContext()$path), 
              "/dataset/smartphones.csv", sep = "")
 dfa<- read_csv2(PATH)
 
+# formatage des types
 dfa$Année <- format(as.POSIXlt(dfa$`date début commercialisation`,tz = "", "%d/%m/%Y"), format="%Y")
-
-
 dfa$fingerprint <- as.numeric(dfa$fingerprint)
 dfa$proximité <- as.numeric(dfa$proximité)
 dfa$accéléromètre <- as.numeric(dfa$accéléromètre)
@@ -27,6 +28,7 @@ dfa$tethering <- as.numeric(dfa$tethering)
 dfa$VoLTE <- as.numeric(dfa$VoLTE)
 dfa$OTA <- as.numeric(dfa$OTA)
 
+# dans ce jeu de données lorsqu'un composant est noté NAN c'est qu'il n'est pas trouvé, autrement dit, il est pas disponible -> 0
 dfa$gyroscope[is.na(dfa$gyroscope)] <- 0
 dfa$baromètre[is.na(dfa$baromètre)] <- 0
 dfa$accéléromètre[is.na(dfa$accéléromètre)] <- 0
@@ -36,22 +38,24 @@ dfa$proximité[is.na(dfa$proximité)] <- 0
 dfa$`computer sync`[is.na(dfa$`computer sync`)] <- 0
 dfa$`OTA`[is.na(dfa$`OTA`)] <- 0
 
+
+# aggrégation par année selon les moyennes
 df<-ddply(dfa, "Année", summarize, fingerprint=mean(`fingerprint`),lumière=mean(`lumière d'ambiance`),proximité=mean(`proximité`),accéléromètre=mean(`accéléromètre`),boussole=mean(`Boussole`),gyroscope=mean(`gyroscope`),baromètre=mean(`baromètre`),NFC=mean(`NFC`),radio=mean(`radio FM`),OTA=mean(`OTA`),sync=mean(`computer sync`))#,bluetooth=mean(`bluetooth`)) 
 
 
+data <-rbind(rep(1,11) , rep(0,11) , df)  %>% select(-Année) # on mets les lignes de max (ligne 1 <- ligne de 1) et de min (ligne 2 <- ligne de 0)
+data <- data[-3,] # on enleve la ligne 3 -> 2010 année trop peu renseignée 
 
-
-
-data <-rbind(rep(1,11) , rep(0,11) , df)  %>% select(-Année)
-data <- data[-3,]
+# définition des couleurs
 colors_border=colormap(colormap=colormaps$viridis, nshades=10, alpha=1)
 colors_in=colormap(colormap=colormaps$viridis, nshades=10, alpha=0.3)
 
+# partage des zones de graphiques
 par(mar=rep(0.8,4))
 par(mfrow=c(2,5))     #en 2*5 -> 10 parties
 
 
-# Loop for each plot
+# boucle pour chaque affichage
 for(i in 1:10){
 
   radarchart( data[c(1,2,i+2),], axistype=1, 
@@ -59,10 +63,10 @@ for(i in 1:10){
               pcol=colors_border[i] , pfcol=colors_in[i] , plwd=4, plty=1 , 
               cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,1,5), cglwd=0.8,
               
-              #custom labels
+              #label personalisé
               vlcex=1.05,
               
-              #title
+              #titre
               title=df[-1,]$Année[i]
   )
 }
